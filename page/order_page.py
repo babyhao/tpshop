718858c27f2485d07681b8b8a3ad15afee26d110
Hwang <hh6228525@163.com>
2020-05-26 22:43:20 +0800

    完成75%

--- page/order_page.py
+++
@@ -1,9 +0,0 @@
-import sys, os
-
-sys.path.append(os.getcwd())
-from appium.webdriver.common.mobileby import MobileBy
-from base.base_action import BaseAction
-
-
-class OrderPage(BaseAction):
-    pass
